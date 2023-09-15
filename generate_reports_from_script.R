68364aa523739a0b2be066a127ac377276a910c9
Kate Miller <kathryn_miller@nps.gov>
2023-09-15 14:36:17 -0400

    filepath update

--- generate_reports_from_script.R
+++ generate_reports_from_script.R
@@ -1,14 +1,18 @@
 
 # Knit park-level reports
-rmarkdown::render('park_summary.Rmd',
-                  output_file = "BOHA_summary.html",
+filepath = "C:/NETN/R_Dev/rockyintertidal_reports/"
+
+rmarkdown::render(paste0(filepath, 'park_summary.Rmd'),
+                  output_file = paste0(filepath, "BOHA_summary.html"),
                   params = list(year_curr = 2021,
                                 park = "BOHA",
                                 all_years = TRUE))
 
 
-rmarkdown::render('park_summary.Rmd',
-                  output_file = "ACAD_summary.html",
+rmarkdown::render(paste0(filepath, 'park_summary.Rmd'),
+                  output_file = paste0(filepath, "ACAD_summary.html"),
                   params = list(year_curr = 2021,
                                 park = "ACAD",
                                 all_years = TRUE))
+
+
